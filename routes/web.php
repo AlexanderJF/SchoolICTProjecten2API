f84e9b7d6f12b4aee7090f0c26714cd16a25a4af
Jordy Neefs <git@jordyneefs.be>
2023-04-17 12:30:00 +0200

    Update Users

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('api/v1')->group(function () {
+    Route::resource('/users', 'App\Http\Controllers\UserController');
+});
